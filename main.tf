8e93016029b1276bda17021b4844a7c6f16ab6ce
Brad Flood <bflood@keyholesoftware.com>
2019-08-25 19:47:53 -0500

    initial configuration

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "fcbh_dbp_api_dev_label" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?tags/0.4.0"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.4.0"
   namespace  = "fcbh"
   stage      = "dev"
   name       = "beanstalk"
@@ -17,67 +17,56 @@ module "fcbh_dbp_api_dev_label" {
   }
 }
 
-/* 
-variable "max_availability_zones" {
-  default = "2"
-}
 
-variable "zone_id" {
-  type        = "string"
-  description = "Route53 Zone ID"
-}
 
 data "aws_availability_zones" "available" {}
 
 module "vpc" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=master"
-  namespace  = "eg"
-  stage      = "dev"
-  name       = "test"
+  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.8.0"
+  namespace  = module.fcbh_dbp_api_dev_label.namespace
+  stage      = module.fcbh_dbp_api_dev_label.stage
+  name       = module.fcbh_dbp_api_dev_label.name
   cidr_block = "10.0.0.0/16"
 }
 
+
 module "subnets" {
-  source              = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=master"
+  source              = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.16.0"
   availability_zones  = ["${slice(data.aws_availability_zones.available.names, 0, var.max_availability_zones)}"]
-  namespace           = "eg"
-  stage               = "dev"
-  name                = "test"
-  region              = "us-east-1"
-  vpc_id              = "${module.vpc.vpc_id}"
-  igw_id              = "${module.vpc.igw_id}"
-  cidr_block          = "${module.vpc.vpc_cidr_block}"
+  namespace           = module.fcbh_dbp_api_dev_label.namespace
+  stage               = module.fcbh_dbp_api_dev_label.stage
+  name                = module.fcbh_dbp_api_dev_label.name
+  region              = var.region
+  vpc_id              = module.vpc.vpc_id
+  igw_id              = module.vpc.igw_id
+  cidr_block          = module.vpc.vpc_cidr_block
   nat_gateway_enabled = "true"
 }
 
 module "elastic_beanstalk_application" {
-  source      = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-application.git?ref=master"
-  namespace   = "eg"
-  stage       = "dev"
-  name        = "test"
-  description = "Test elastic_beanstalk_application"
+  source      = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-application.git?ref=tags/0.1.6"
+  namespace   = module.fcbh_dbp_api_dev_label.namespace
+  stage       = module.fcbh_dbp_api_dev_label.stage
+  name        = module.fcbh_dbp_api_dev_label.name
+  description = "DBP API Beanstalk application"
 }
 
 module "elastic_beanstalk_environment" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-environment.git?ref=master"
-  namespace = "eg"
-  stage     = "dev"
-  name      = "test"
+  source    = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-environment.git?ref=tags/0.13.0"
+  namespace = module.fcbh_dbp_api_dev_label.namespace
+  stage     = module.fcbh_dbp_api_dev_label.stage
+  name      = module.fcbh_dbp_api_dev_label.name
   zone_id   = "${var.zone_id}"
   app       = "${module.elastic_beanstalk_application.app_name}"
 
-  instance_type           = "t2.small"
-  autoscale_min           = 1
-  autoscale_max           = 2
-  updating_min_in_service = 0
-  updating_max_batch      = 1
+  instance_type           = var.instance_type
 
   loadbalancer_type   = "application"
-  vpc_id              = "${module.vpc.vpc_id}"
-  public_subnets      = "${module.subnets.public_subnet_ids}"
-  private_subnets     = "${module.subnets.private_subnet_ids}"
-  security_groups     = ["${module.vpc.vpc_default_security_group_id}"]
-  solution_stack_name = "64bit Amazon Linux 2018.03 v2.12.10 running Docker 18.06.1-ce"
+  vpc_id              = module.vpc.vpc_id
+  public_subnets      = module.subnets.public_subnet_ids
+  private_subnets     = module.subnets.private_subnet_ids
+  security_groups     = [module.vpc.vpc_default_security_group_id]
+  solution_stack_name = var.solution_stack_name
   keypair             = ""
 
   env_vars = "${
@@ -89,4 +78,3 @@ module "elastic_beanstalk_environment" {
   }"
 }
 
- */
--- variables.tf
+++ variables.tf
@@ -5,3 +5,20 @@ variable "region" {
 variable "amis" {
   type = "map"
 }
+
+variable "max_availability_zones" {
+  default = "2"
+}
+
+variable "zone_id" {
+  type        = "string"
+  description = "Route53 Zone ID"
+}
+
+variable "instance_type" {
+  default = "t3.medium"
+}
+
+variable "solution_stack_name" {
+  default = "64bit Amazon Linux 2018.03 v2.8.14 running PHP 7.2"
+}
